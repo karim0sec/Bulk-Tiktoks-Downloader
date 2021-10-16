2fd91e864f786c8b2c8cca1674af24f6c86c7e5d
Karim elsayed <92269255+Karim-Arab@users.noreply.github.com>
2021-10-16 21:52:41 +0200

    now it's download videos directly without links

--- tiktok.js
+++ tiktok.js
@@ -6,21 +6,21 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 //here we catch all requests to block
 //images & stylesheet & fonts to save data and speed up our scraper
-  await page.setRequestInterception(true);
+await page.setRequestInterception(true);
 
-  page.on('request', (request) => {
-    if(['image', 'stylesheet', 'font'].includes(request.resourceType())) {
-      request.art();
-    } else {
-      request.continue();
-    }
-  })
+page.on('request', (request) => {
+  if(['image', 'stylesheet', 'font'].includes(request.resourceType())) {
+    request.abort();
+  } else {
+    request.continue();
+  }
+})
 
   //here we change user agent to not detect as bot with waf
   await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.156 Safari/537.36");
-  await page.goto('https://www.tiktok.com/'); //go for tiktok
-  await page.waitForSelector('input[name="q"]');//select search bar
-  await page.type('input[name="q"]' , 'asmr', { delay: 100 }); // asmr is our keyword u can change it depend on content u want
+  await page.goto('https://www.tiktok.com/');
+  await page.waitForSelector('input[name="q"]');
+  await page.type('input[name="q"]' , 'love' , { delay: 100 }); // change love for keyword of content
   await page.keyboard.press('Enter'); //press enter
   await page.waitForNavigation();//wait page load
   await page.waitForSelector('div.tiktok-bbkab3-DivContainer > div > div > a')//select 12 links & 12 video description
@@ -39,7 +39,8 @@ const puppeteer = require('puppeteer');
   if (err) throw err;
   console.log('Saved Videos names');
   });
-//loop on snaptik for no watermark tiktok videos    
+//loop on snaptik for no watermark tiktok videos
+  
 for (var i=0;i<12;i++) //Loop 10 times
 {
 
@@ -61,15 +62,30 @@ const featureArticle = (await page.$x('//*[@id="download-block"]/div/a[1]'))[0];
         return el.href;
     }, featureArticle);
     var noWaterMark = text
-    const content = noWaterMark;
-  //save links to files and ready to send to our down.py app
-    const fs = require('fs');
-  
-    fs.appendFile('newapp.txt', 'file_url_1 = "' + content  + '"' + "\r\n" , function (err) {
-      if (err) throw err;
-      console.log('Saved!'); 
+    const content = decodeURIComponent(noWaterMark);
+
+    
+    const https = require('https');
+    const ds = require('fs');
+    
+     // link to file you want to download
+    const path = './' // location to save videos
+
+    //genrate random number for file name
+    function getRandomFileName() {
+      var timestamp = new Date().toISOString().replace(/[-:.]/g,"");    
+      return timestamp;
+      }
+    const request = https.get(content, function(response) {
+        if (response.statusCode === 200) {
+            var file = ds.createWriteStream(path+getRandomFileName()+'.mp4');
+            response.pipe(file);
+            console.log('Saved!');
+        }
+        request.setTimeout(60000, function() { // if after 60s file not downlaoded, we abort a request 
+            request.destroy();
     });
-//all result now saved in newapp.txt
+    });
     ;};
 
 
